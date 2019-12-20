2bf1c4aadc2511a9041bbf84d31a53e1178bc175
Misaki Koonce <misaki.koonce@gmail.com>
2019-12-20 13:42:21 -0800

    Add UI logic - RepeatGame method.

--- WordCounter/WordCounter.cs
+++ WordCounter/WordCounter.cs
@@ -7,21 +7,37 @@ namespace WordCounter.Models
     {
         static void Main()
         {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Welcome to the Word Counter! \nPlease input a word and a sentence, and we will count how many of the word is used in the sentence.");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             string word = TakeWordInput();
             string sentence = TakeSentenceInput();
             RepeatCounter userCounter = new RepeatCounter(word, sentence);
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine($"\n{userCounter.CountWord()}");
+            int result = userCounter.CountWord();
+            if(result == 0)
+            {
+                Console.WriteLine("There was no match in the sentence.");
+            }
+            else if (result == 1)
+            {
+                Console.WriteLine($"The word is used {result} time in the sentence.");
+            }
+            else
+            {
+                Console.WriteLine($"The word is used {result} times in the sentence.");
+            }
             Console.ResetColor();
-            Main();
+            RepeatGame();
         }
 
+        // Make both user input lower case and trim the white space in the end.
         private static string TakeWordInput()
         {
             Console.WriteLine("\nPlease enter a word:");
-            string wordInput = Console.ReadLine().ToLower();
-            bool isWordInputValid = RepeatCounter.CheckValidInput(wordInput);
-            if(!isWordInputValid)
+            string wordInput = Console.ReadLine().ToLower().Trim();
+            bool isValid = RepeatCounter.CheckValidInput(wordInput);
+            if(!isValid)
             {
                 ErrorMessage();
                 TakeWordInput();
@@ -32,14 +48,34 @@ namespace WordCounter.Models
         private static string TakeSentenceInput()
         {
             Console.WriteLine("\nPlease enter a sentence:");
-            string sentenceInput = Console.ReadLine().ToLower();
-            bool isSentenceInputValid = RepeatCounter.CheckValidInput(sentenceInput);
-            if(!isSentenceInputValid)
+            string sentenceInput = Console.ReadLine().ToLower().Trim();
+            bool isValid = RepeatCounter.CheckValidInput(sentenceInput);
+            if(!isValid)
             {
                 ErrorMessage();
                 TakeSentenceInput();
             }
             return sentenceInput;
+        }
+
+        private static void RepeatGame()
+        {
+            Console.WriteLine("\nDo you want to play again? (Yes / No)");
+            string userInput = Console.ReadLine().ToLower().Trim();
+            bool isValid = RepeatCounter.CheckValidInput(userInput);
+            if(!isValid || (userInput != "yes" && userInput != "no"))
+            {
+                ErrorMessage();
+                RepeatGame();
+            }
+            else if (userInput == "yes")
+            {
+                Main();
+            }
+            else
+            {
+                Console.WriteLine("See you next time!");
+            }
         }
 
         private static void ErrorMessage()
